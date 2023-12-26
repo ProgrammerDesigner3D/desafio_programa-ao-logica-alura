58e3bbd40cb610da69fd0518b70577a971375aea
Ricardo Felix <programmer.designer.3d@gmail.com>
2023-12-26 05:39:09 -0300

    Acabando primeira parte do desafio aluga Games

---
+++ alugames/js/app.js
@@ -0,0 +1,15 @@
+function alterarStatus(id){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let image = gameClicado.querySelector(' .dashboard__item__img');
+    let botao = gameClicado.querySelector(' .dashboard__item__button');
+    
+    if(image.classList.contains('dashboard__item__img--rented')){
+        image.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    }else{
+        image.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+}
