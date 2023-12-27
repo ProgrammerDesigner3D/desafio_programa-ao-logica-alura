7d0609f8b19ccda8ca3c91aa6577b79ffddfaeab
Ricardo Felix <programmer.designer.3d@gmail.com>
2023-12-27 13:11:22 -0300

    Criando a função do palindromo

--- alugames/js/palindromo.js
+++ alugames/js/palindromo.js
@@ -6,9 +6,9 @@ function essaPalavraEUmPalindromo(){
     var palavraInvertida = separandoAsLetrasDaPalavra.reverse();
     palavraInvertida = palavraInvertida.join("");
     if(palavra == palavraInvertida){
-        console.log("A palvra " + palavra + " é um palíndromo");
+        console.log("A palavra " + palavra + " é um palíndromo");
     }else{
-        console.log("A palvra " + palavra + " não é um palíndromo");
+        console.log("A palavra " + palavra + " não é um palíndromo");
     }
 
 }
