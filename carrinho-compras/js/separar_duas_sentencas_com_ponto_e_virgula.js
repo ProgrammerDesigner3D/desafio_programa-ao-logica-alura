10d121e4881fbcc5fd782a69bf5c22e18167f8de
Ricardo Felix <programmer.designer.3d@gmail.com>
2024-01-08 19:27:05 -0300

    Estudando casos e arrumando e melhorando

--- carrinho-compras/js/separar_duas_sentencas_com_ponto_e_virgula.js
+++ carrinho-compras/js/separar_duas_sentencas_com_ponto_e_virgula.js
@@ -2,7 +2,7 @@ function separaFrase() {
     
 
 const duasSentencas = document.getElementById('frase').value;
-const frasesSeparadas = duasSentencas.split(',');
+const frasesSeparadas = duasSentencas.split(';');
 
 document.getElementById('fraseSeparada').textContent = `Resultado da frase separada: ${frasesSeparadas}`;
 
