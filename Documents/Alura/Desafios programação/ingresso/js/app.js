1882ead142f5428b32622cb975b8a54b227d9d19
Ricardo Felix <programmer.designer.3d@gmail.com>
2024-01-13 04:42:53 -0300

    Incrementando o desafio

--- Documents/Alura/Desafios programação/ingresso/js/app.js
+++ Documents/Alura/Desafios programação/ingresso/js/app.js
@@ -1,30 +1,37 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = parseInt(document.getElementById('qtd').value);
+    let qtdInput = document.getElementById('qtd');
+    let qtd = parseInt(qtdInput.value);
 
-    //alert(tipo.value);
-    //alert(qtd.value);
+
+    // Verificar se a quantidade é um número positivo
+    if (isNaN(qtd) || qtd <= 0) {
+        alert('Por favor, insira uma quantidade válida.');
+        return;
+    }
+
 
     if (tipo.value == 'pista') {
         comprarPista(qtd);
-    }else if (tipo.value == 'superior') {
+    } else if (tipo.value == 'superior') {
         comprarSuperior(qtd);
     } else {
         comprarInferior(qtd);
-    } 
-
+    }
 }
+
 
 function comprarPista(qtd) {
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
-    if(qtd > qtdPista){
+    if (qtd > qtdPista) {
         alert('Quantidade indisponível para tipo pista');
-    }else{
+    } else {
         qtdPista = qtdPista - qtd;
         document.getElementById('qtd-pista').textContent = qtdPista;
         alert('Compra realizada com sucesso!');
     }
 }
+
 
 function comprarSuperior(qtd) {
     let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
@@ -35,8 +42,8 @@ function comprarSuperior(qtd) {
         document.getElementById('qtd-superior').textContent = qtdSuperior;
         alert('Compra realizada com sucesso!');
     }
-
 }
+
 
 function comprarInferior(qtd) {
     let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
