5df0a434d8f43148bc9d9ce951b9c445c1d349a9
Ricardo Felix <programmer.designer.3d@gmail.com>
2024-01-16 13:16:42 -0300

    Incrementando a saída da informação na página HTML

--- Documents/Alura/Desafios programação/ingresso/js/converter_temperatura.js
+++ Documents/Alura/Desafios programação/ingresso/js/converter_temperatura.js
@@ -2,14 +2,20 @@
 function converterTemperatura() {
     var tnum = document.getElementById("tnum").value;
     var esc = document.getElementById("esc").options[document.getElementById("esc").selectedIndex].value;
+    var fahr = (tnum * 9/5) + 32;
+    var cels = (tnum - 32) * 5/9;  
     
     if (esc.toLowerCase() === 'celsius') {
-        alert((tnum * 9/5) + 32 + " Graus em Fahrenheit");
+        document.getElementById("saida").innerHTML = "Convesão de " + tnum + " Celsius para Fahrenheit = " + fahr + " Graus";
+        //alert(fahr + " Graus em Fahrenheit");
         document.getElementById("tnum").value = "";
+        tnum = 0;
         // Celsius para Fahrenheit        
     } else if (esc.toLowerCase() === 'fahrenheit') {
-        alert((tnum - 32) * 5/9 + " Graus em Celsius");        
+        document.getElementById("saida").innerHTML = "Convesão de " + tnum + " Fahrenheit para Celsius = " + cels + " Graus";
+        //alert(cels + " Graus em Celsius");        
         document.getElementById("tnum").value = "";
+        tnum = 0;
         // Fahrenheit para Celsius
     }
 }
