53ee757c3e7a37ec578b91b137e1122e2647dd10
Ricardo Felix <programmer.designer.3d@gmail.com>
2024-01-24 17:13:30 -0300

    Implementando o codigo com novas validações

--- Documents/Alura/Desafios programação/amigo-secreto/js/app.js
+++ Documents/Alura/Desafios programação/amigo-secreto/js/app.js
@@ -2,6 +2,16 @@ let amigos = [];
 
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
+    if (amigo.value == '') {
+        alert('Informe o nome do amigo!');
+        return;
+    }
+
+    if (amigos.includes(amigo.value)) {
+        alert('Nome já adicionado!');
+        return;
+    }
+
     let lista = document.getElementById('lista-amigos');
 
     amigos.push(amigo.value);
@@ -29,6 +39,11 @@ function embaralhar(lista) {
 }
 
 function sortear() {
+
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos');
+        return;
+    }
 
     embaralhar(amigos);
 
