be22270e881e2e09634f5abef042c87205f34391
Ricardo Felix <programmer.designer.3d@gmail.com>
2024-02-01 00:53:02 -0300

    Implemento dos desafios com interface em html

--- Documents/Alura/Desafios programação/amigo-secreto/js/tamanho_array.js
+++ Documents/Alura/Desafios programação/amigo-secreto/js/tamanho_array.js
@@ -1,7 +1,12 @@
 let gavetas = [];
 
 function adicionandoConteudo() {
-    let inserindo = document.getElementById("entrada").value;
+    let inserindo = document.getElementById("entrada");
+    //alert(inserindo.value.trim());
+    if (inserindo.value.trim() === '' || inserindo.value.trim() === null){//tratamento para quando tentar usar espaço
+        
+        return alert("Por favor, não utilize espaço como opção digite algo");
+    }
     gavetas.push(inserindo.value);
     document.getElementById("entrada").value = '';    
 }
@@ -13,5 +18,31 @@ function verificaTamanho(){
 
 function apagaMedia() {
     document.getElementById("resultado").innerHTML = "";
+    document.getElementById("mostrArray").innerHTML ="";
+    document.getElementById('lista-add').innerHTML = '';
     gavetas.length = 0;
+}
+
+function mostraConteudo() {
+    document.getElementById("mostrArray").innerHTML = (gavetas.join('-'));
+}
+
+function mostrarPoiscaoElemento() {
+
+    let lista = document.getElementById('lista-add');
+        
+    lista.innerHTML = '';
+
+    for(let i = 0; i < gavetas.length; i++){
+        let paragrafo = document.createElement('p');
+        paragrafo.textContent = gavetas[i]=i + "  " + " - " + "  " + gavetas[i];
+
+        paragrafo.addEventListener('click', function(){
+            excluirAmigo(i);
+        });
+
+        lista.appendChild(paragrafo);
+
+    }
+
 }
